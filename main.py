d00f99b0f94d319c3bb0f9e309cf5958260c4e7c
somesh hiremath <someshhiremath9@yahoo.com>
2024-08-11 22:38:23 +0530

    params and query

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 class Item(BaseModel):
     name: str
     price: float
@@ -14,6 +16,10 @@ class Item(BaseModel):
 async def read_root():
     return {"Hello": "World"}
 
+@app.get("/items", description="This is List all items")
+async def list_items(skip: int=0, limit: int=10):
+    return fake_items_db[skip : skip + limit]
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
@@ -22,3 +28,21 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 async def put_item(item_id: int, item:Item):
     return {"item_name": item.name, "item_id":item_id}
+
+@app.get("/items_filter/{items_id}")
+async def get_item(item_id: int, q: str | None =None):
+    if q:
+        return {"items_id": item_id, "q": q}
+    return {"item_id":item_id}
+
+
+@app.get("/items_list/extra_filter")
+async def filter_data(item_id: int, sample_query: str, q: str | None = None, short: bool = False):
+    items = {"item_id": item_id, "sample_query":sample_query}
+    if q:
+        items.update({"query": q})
+    if not short:
+        items.update({
+            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id."
+        })
+    return items
